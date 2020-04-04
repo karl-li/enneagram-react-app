c2667114d6018198817c941d6cac3bddece96f9a
draintheshallows <kli@quilhealth.com>
2020-04-04 08:33:45 -0400

    Stylin the front page brah

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import logo from './9arch-logo.svg';
+import logo from './img/9arch-logo.svg';
+import menu from './img/Icon-Menu.svg'
+import heroImg from './img/enneagram-black.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <header><img src={logo} width="32"/></header>
-      <h1>9 Archetypes</h1>
-      <h2>What is the Enneagram?</h2>
-      <p>The Enneagram is a 9-sided figure that represents 9 different personas. It is a profound tool for self-discovery and illuminating your true self.</p>
-      <button>Take the test</button>
-      <h2>The 9 Archetypes</h2>
+    <div className="container">
+      <div className="nav">
+        <img src={logo} width="32"/>
+        <h1>9 Archetypes</h1>
+        <img className="icon-menu" src={menu} width="32" height="32"/>
+      </div>
+      <section className="hero">
+        <img className="heroImg" src={heroImg} width="180" height="180"/>
+        <h2>What is the Enneagram?</h2>
+        <p>The Enneagram is a 9-sided figure that represents 9 different personas. It is a profound tool for self-discovery and illuminating your true self.</p>
+        <button>Take the test</button>
+        </section>
+        <h2>The 9 Archetypes</h2>
+      <footer>©2020 Karl Li. All Rights Reserved.</footer>
     </div>
   );
 }
